2d476e6c8fb623395f5ccbb71c6c3a57d987c18a
volto9 <volto9@yahoo.com>
2024-10-29 11:15:06 +0000

    serv-k

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-import express from "express";
-import cors from "cors";
-import pg from "pg";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import pg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-app.get("/", (request, response) => response.json("its working!"));
+app.get('/', (request, response) => response.json('its working!'));
 
-app.listen(8080, () => console.log("App is running on PORT 8080"));
+app.get('/travel', async function (request, response) {
+  const result = await db.query('SELECT * FROM travel');
+  const travel = result.rows;
+  response.json(travel);
+});
+
+app.listen(8080, () => console.log('App is running on PORT 8080'));
