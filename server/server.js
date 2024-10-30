a411ee9d8fd0e0871043185a66233fdf32bf722f
Ahmed Osman <a.osman2024@gmail.com>
2024-10-30 09:20:16 +0000

    Added POST functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import pg from 'pg';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,12 +10,21 @@ app.use(cors());
 app.use(express.json());
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-app.get('/', (request, response) => response.json('its working!'));
+app.get("/", (request, response) => response.json("its working!"));
 
-app.get('/travel', async function (request, response) {
-  const result = await db.query('SELECT * FROM travel');
+app.get("/travel", async function (request, response) {
+  const result = await db.query("SELECT * FROM travel");
   const travel = result.rows;
   response.json(travel);
 });
 
-app.listen(8080, () => console.log('App is running on PORT 8080'));
+app.post("/travel", async (request, response) => {
+  const { name, city, review } = request.body;
+  const result = await db.query(
+    "INSERT INTO travel (name, city, review) VALUES ($1, $2, $3) RETURNING *",
+    [name, city, review]
+  );
+  response.json(result.rows);
+});
+
+app.listen(8080, () => console.log("App is running on PORT 8080"));
