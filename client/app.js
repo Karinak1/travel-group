f1210461b8663cf52641b3b8cfc80d2181b1be76
volto9 <volto9@yahoo.com>
2024-10-31 09:55:46 +0000

    duba

--- client/app.js
+++ client/app.js
@@ -1,13 +1,13 @@
-const imageBg = document.getElementsByClassName("background-image");
+const imageBg = document.getElementsByClassName('background-image');
 
 const backgroundImages = [
-  "https://static.independent.co.uk/2023/08/02/10/iStock-1248448159%20%281%29.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSidQZFrMzrV94fwOxlf_MjepdxAwrEV9GNRg&s",
-  "https://dotravel.com/uploads/articles/95/april-holiday-destinations_3.jpg",
+  'https://static.independent.co.uk/2023/08/02/10/iStock-1248448159%20%281%29.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSidQZFrMzrV94fwOxlf_MjepdxAwrEV9GNRg&s',
+  'https://dotravel.com/uploads/articles/95/april-holiday-destinations_3.jpg',
 ];
 
 let index = 0;
-const img = document.querySelector("#bg");
+const img = document.querySelector('#bg');
 function changeBg() {
   img.src = backgroundImages[index];
   index > 1 ? (index = 0) : index++;
@@ -15,7 +15,7 @@ function changeBg() {
 
 setInterval(changeBg, 3000);
 
-const travelContainer = document.getElementsByClassName("places"); //form div className
+const travelContainer = document.getElementsByClassName('places'); //form div className
 async function SubmitReview(event) {
   event.preventDefault();
   const formData = new FormData(formInfo);
@@ -23,9 +23,9 @@ async function SubmitReview(event) {
 }
 
 async function getTravel() {
-  const response = await fetch("http://localhost:8080/travel", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const response = await fetch('http://localhost:8080/travel', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
   });
   const guests = await response.json();
@@ -36,7 +36,7 @@ for (let i = 0; i < travel.length; i++) {
   const review = travel[i].review;
   const city = travel[i].city;
 
-  const p = document.createElement("p");
+  const p = document.createElement('p');
 
   p.textContent = `${city} ${review} by ${name}  `;
 
