6dbef1b32380b5314ce56142d46e02c1efa1fefa
Barmington Chuku <barmingtonk@gmail.com>
2024-10-31 14:44:30 +0000

    adjust

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,18 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-
+        main: resolve(__dirname, "index.html"),
+        pageLA: resolve(__dirname, "pageLA/pageLA.index.html"),
+        pageBerlin: resolve(__dirname, "pageBerlin/pageBerlin.index.html"),
+        pageNewyork: resolve(__dirname, "pageNewyork/pageNewyork.index.html"),
+        pageDoha: resolve(__dirname, "pageDoha/index.html"),
+        pageCairo: resolve(__dirname, "pageCairo/index.html"),
+        pageIstanbul: resolve(__dirname, "pageIstanbul/index.html"),
+      },
+    },
+  },
+});
