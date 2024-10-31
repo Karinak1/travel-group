11b8dd7d82175b587d19e91cd129e958db0eb326
Ahmed Osman <a.osman2024@gmail.com>
2024-10-31 10:18:32 +0000

    vite updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-
+        main: resolve(__dirname, "index.html"),
+        pageLA: resolve(__dirname, "pageLA/pageLA.index.html"),
+        pageBerlin: resolve(__dirname, "pageBerlin/pageBerlin.index.html"),
+        pageNewyork: resolve(__dirname, "pageNewyork/pageNewyork.index.html"),
+        pageDubai: resolve(__dirname, "pageDubai/pageDubai.index.html"),
+      },
+    },
+  },
+});
