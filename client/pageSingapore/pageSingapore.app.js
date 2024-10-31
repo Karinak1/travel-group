7db27cdaf8a85db3f546398a2bf7e1f24895d9c0
Khushi Sukar <khushisukar234@gmail.com>
2024-10-31 15:04:07 +0000

    update Js

--- client/pageSingapore/pageSingapore.app.js
+++ client/pageSingapore/pageSingapore.app.js
@@ -7,6 +7,30 @@ document
     const city = this.city.value.trim();
     const review = this.review.value.trim();
 
+    async function getReview(name, city, review){
+      const response = await fetch('http://localhost:8080/travel',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, city, review }),
+      });
+      const messages = await response.json();
+      for (let i = 0; i < messages.length; i++) {
+        
+        const name = messages[i].name;
+        const city = messages[i].city;
+        const review = messages[i].review;
+        console.log(messages)
+        console.log(name, city, review)
+        const p = document.createElement('p');
+        p.textContent = `${review} by ${name}`;
+        msgContainer.appendChild(p)
+        
+}}
+    getReview(name, city, review)
+    
+
     const reviewItem = document.createElement("li");
     reviewItem.textContent = `${name} from ${city}: ${review}`;
 
@@ -20,6 +44,6 @@ document
 
     reviewItem.appendChild(deleteButton);
     document.getElementById("review-list").appendChild(reviewItem);
-
-    this.reset();
+        this.reset();
+    
   });
