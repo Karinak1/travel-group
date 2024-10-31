3e6528c00bd52e96db975a610126557ed3cd4d9e
Barmington Chuku <barmingtonk@gmail.com>
2024-10-31 15:31:46 +0000

    changes

--- client/pageIstanbul/app.js
+++ client/pageIstanbul/app.js
@@ -3,9 +3,31 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const name = this.name.value;
-    const city = this.city.value;
-    const review = this.review.value;
+    const name = this.name.value.trim();
+    const city = this.city.value.trim();
+    const review = this.review.value.trim();
+
+    async function getReview(name, city, review) {
+      const response = await fetch("http://localhost:8080/travel", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, city, review }),
+      });
+      const messages = await response.json();
+      for (let i = 0; i < messages.length; i++) {
+        const name = messages[i].name;
+        const city = messages[i].city;
+        const review = messages[i].review;
+        console.log(messages);
+        console.log(name, city, review);
+        const p = document.createElement("p");
+        p.textContent = `${review} by ${name}`;
+        msgContainer.appendChild(p);
+      }
+    }
+    getReview(name, city, review);
 
     const reviewItem = document.createElement("li");
     reviewItem.textContent = `${name} from ${city}: ${review}`;
@@ -13,13 +35,12 @@ document
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.style.marginLeft = "10px";
+
     deleteButton.addEventListener("click", function () {
       reviewItem.remove();
     });
 
     reviewItem.appendChild(deleteButton);
-
     document.getElementById("review-list").appendChild(reviewItem);
-
     this.reset();
   });
