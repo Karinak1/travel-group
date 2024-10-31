8e2d02f9bed6497476bb4de5adbada2d1b04dca1
Khushi Sukar <khushisukar234@gmail.com>
2024-10-31 11:08:19 +0000

    page changes

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
  document.getElementsByClassName("background-image");
 
 const backgroundImages = [
-  "https://static.independent.co.uk/2023/08/02/10/iStock-1248448159%20%281%29.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSidQZFrMzrV94fwOxlf_MjepdxAwrEV9GNRg&s",
+  "https://img.freepik.com/free-photo/full-shot-travel-concept-with-landmarks_23-2149153258.jpg",
+  "https://www.wiredforadventure.com/wp-content/uploads/2019/08/hendrik-cornelissen-qrcOR33ErA-unsplash-scaled.jpg",
   "https://dotravel.com/uploads/articles/95/april-holiday-destinations_3.jpg",
 ];
 
@@ -16,34 +16,5 @@ function changeBg() {
 setInterval(changeBg, 3000);
 
 const travelContainer = document.getElementsByClassName("places"); //form div className
-async function SubmitReview(event) {
-    event.preventDefault();
-    const formData = new FormData(formInfo);
-    const body = Object.fromEntries(formData);
-    console.log(body)
 
-
-    async function getTravel() {
-
-        const response = await fetch('http://localhost:8080/travel', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body),
-        });
-        const travel = await response.json();
-        console.log(travel)
-        // for (let i = 0; i < travel.length; i++) {
-        //   const name = travel[i].name;
-        //   const review = travel[i].review;
-        //   const city = travel[i].city;
-
-        //   const p = document.createElement('p');
-
-        //   p.textContent = `${city} ${review} by ${name}  `;
-
-        //   places.appendChild(p);
-        // }
-    }
-    // getTravel();
-}
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,20 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-)}
+        pageLA: resolve(__dirname, "pageLA/pageLA.index.html"),
+        pageBerlin: resolve(__dirname, "pageBerlin/pageBerlin.index.html"),
+        pageNewyork: resolve(__dirname, "pageNewyork/pageNewyork.index.html"),
+        pageBangkok: resolve(__dirname, "./pageBangkok/pageBankok.index.html"),
+        pageTokyo: resolve(__dirname, "./pageTokyo/pageTokyo.index.html"),
+        pageSingapore: resolve(__dirname, "./pageSingapore/pageSingapore.index.html"),
+      },
+    },
+  },
+});
+
